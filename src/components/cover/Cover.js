7989b5013e2d9f2b1d9d745363af5bd76f362965
joalvarado2 <jonathanalva92@gmail.com>
2021-03-01 19:05:21 -0500

    terminando acabados del footer y dejando la pagina responsive

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./cover.css";
-import coverVideo from "../../media/coverVideo.mp4";
+import Video1 from "../../media/Video1.mp4";
 
 const Cover = () => {
     return (
         <div className="cover-container">
-          <video className="video" src={coverVideo} autoPlay loop muted />
+          <video className="video" src={Video1} autoPlay loop muted />
           <h1>Jonathan Alvarado</h1>
           <p>Estudiante de ingenieria de software | Desarrollador Frontend | </p>
         </div>
